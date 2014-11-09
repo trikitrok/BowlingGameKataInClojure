5bf868f491e139fb45ecb2b376056505a142b058
trikitrok <trikitrok@gmail.com>
2014-11-09 11:51:04 +0100

    It scores games with spares

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -6,11 +6,19 @@
 (defn first-frame [rolls]
   (take 2 rolls))
 
+(defn spare? [rolls]
+  (= 10 (points (first-frame rolls))))
+
+(defn bonus-rolls [rolls]
+  (cond (spare? rolls) (drop 2 (take 3 rolls))
+        :else (take 0 rolls)))
+
 (defn rest-frames [rolls]
   (drop 2 rolls))
 
 (defn score-first-frame [rolls]
-  (+ (points (first-frame rolls))))
+  (+ (points (first-frame rolls))
+     (points (bonus-rolls rolls))))
 
 (defn score [rolls]
   (if (empty? rolls)
--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -4,10 +4,14 @@
 
 (def a-gutter-game (repeat 20 0))
 (def a-no-spares-no-strikes-game (concat [1 6 4 5 3 1] (repeat 14 0)))
+(def a-game-with-spares (concat [4 6 4 5 3 1] (repeat 14 0)))
 
 (facts "about bowling-game"
        (fact "it scores a game with no spins down"
              (score a-gutter-game) => 0)
        
        (fact "it scores a game with neither spares nor strikes"
-             (score a-no-spares-no-strikes-game) => 20))
+             (score a-no-spares-no-strikes-game) => 20)
+       
+       (fact "it scores a game with a spare"
+             (score a-game-with-spares) => 27))
