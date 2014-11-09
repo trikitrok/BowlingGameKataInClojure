4417d90fd83d45dbaff20e8d5d176d329e25e5f8
trikitrok <trikitrok@gmail.com>
2014-11-09 17:29:46 +0100

    Made all helpers private

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -1,42 +1,42 @@
 (ns bowling-game.core)
 
-(defn points [rolls]
+(defn- points [rolls]
   (reduce + rolls))
 
-(def ten-points? 
+(def ^:private ten-points? 
   (comp (partial = 10) points))
 
-(def strike? 
+(def ^:private strike? 
   (comp ten-points? (partial take 1)))
 
-(def spare?
+(def ^:private spare?
   (comp ten-points? (partial take 2)))
 
-(defn first-frame [rolls]
+(defn- first-frame [rolls]
   (if (strike? rolls)
     (take 1 rolls)
     (take 2 rolls)))
 
-(defn take-next [n rolls]
+(defn- take-next [n rolls]
   (drop (- 3 n) (take 3 rolls)))
 
-(defn bonus-rolls [rolls]
+(defn- bonus-rolls [rolls]
   (cond (strike? rolls) (take-next 2 rolls)
         (spare? rolls) (take-next 1 rolls)
         :else (empty rolls)))
 
-(defn rest-frames [rolls]
+(defn- rest-frames [rolls]
   (if (strike? rolls)
     (drop 1 rolls)
     (drop 2 rolls)))
 
-(defn score-current-frame [rolls n]
+(defn- score-current-frame [rolls n]
   (if (> n 10)
     0
     (+ (points (first-frame rolls))
        (points (bonus-rolls rolls)))))
 
-(defn score-frames [rolls n]
+(defn- score-frames [rolls n]
   (if (empty? rolls)
     0
     (+ (score-current-frame rolls n)
