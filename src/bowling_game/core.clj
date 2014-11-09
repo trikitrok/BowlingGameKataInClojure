076fdbc9c87385ad548031590bb017b931327a88
trikitrok <trikitrok@gmail.com>
2014-11-09 11:46:38 +0100

    Refactor: Modified code to make easier to introduce spares in next test

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -1,4 +1,19 @@
 (ns bowling-game.core)
 
-(defn score [rolls]
+(defn points [rolls]
   (reduce + rolls))
+
+(defn first-frame [rolls]
+  (take 2 rolls))
+
+(defn rest-frames [rolls]
+  (drop 2 rolls))
+
+(defn score-first-frame [rolls]
+  (+ (points (first-frame rolls))))
+
+(defn score [rolls]
+  (if (empty? rolls)
+    0
+    (+ (score-first-frame rolls)
+       (score (rest-frames rolls)))))
